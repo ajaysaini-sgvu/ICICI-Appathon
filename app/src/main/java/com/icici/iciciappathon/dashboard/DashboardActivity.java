077b31299e04df04c8a36e781d0d9fe79ba886a9
Ajay Saini <ajaysaini.sgvu@gmail.com>
2017-04-09 12:11:48 +0530

    Add navigation view and app screenshots

--- app/src/main/java/com/icici/iciciappathon/dashboard/DashboardActivity.java
+++ app/src/main/java/com/icici/iciciappathon/dashboard/DashboardActivity.java
@@ -19,16 +19,74 @@
 
 package com.icici.iciciappathon.dashboard;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.AppCompatTextView;
+import android.view.Gravity;
+import android.view.View;
 
 import com.icici.iciciappathon.R;
+import com.icici.iciciappathon.databinding.ActivityDashboardBinding;
+import com.icici.iciciappathon.shopping.ScanBarcodeActivity;
+import com.icici.iciciappathon.ui.BaseActivity;
 
-public class DashboardActivity extends AppCompatActivity {
+public class DashboardActivity extends BaseActivity implements View.OnClickListener {
+
+    private DrawerLayout mDrawerLayout;
+
+    private ActionBarDrawerToggle mActionBarDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
+        ActivityDashboardBinding activityDashboardBinding = setContentView(this, R.layout.activity_dashboard);
+
+        // setting the toolbar title
+        setToolbarText(getString(R.string.home_screen_name));
+
+        ((AppCompatTextView) getToolbar().getChildAt(0)).setGravity(Gravity.START);
+
+        // initializing drawer layout and action bar toggle
+        mDrawerLayout = activityDashboardBinding.drawer;
+        mActionBarDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, getToolbar(), R.string.home_screen_open_drawer, R.string.home_screen_close_drawer) {
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                // Code here will be triggered once the drawer closes as we dont want anything to happen so we leave this blank
+                super.onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                // Code here will be triggered once the drawer open as we dont want anything to happen so we leave this blank
+
+                super.onDrawerOpened(drawerView);
+            }
+        };
+
+        //Setting the actionbarToggle to drawer layout
+        mDrawerLayout.addDrawerListener(mActionBarDrawerToggle);
+
+        //calling sync state is necessay or else your hamburger icon wont show up
+        mActionBarDrawerToggle.syncState();
+
+        NavigationView navigationView = activityDashboardBinding.navigationView;
+
+        activityDashboardBinding.contentGetStarted.getStartedButton.setOnClickListener(this);
+    }
+
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        mActionBarDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(DashboardActivity.this, ScanBarcodeActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
